28b48672b09bb94c1d940d0398b5cd8454e602ef
Cameron White <cameronwhite16@gmail.com>
2019-12-01 13:41:33 -0700

    Adding logger back to part1 solution

--- 2019/day1/part1.soln.js
+++ 2019/day1/part1.soln.js
@@ -8,6 +8,7 @@ function solve() {
     return readInput("../input/day1/input")
     .then((massList) => computeTotalMass(massList, computeFuelForModule))
     .then((result) => console.log(`Day 1, Part 1 Total Fuel: ${result}`))
+    .catch(logError);
 }
 
 module.exports = {
