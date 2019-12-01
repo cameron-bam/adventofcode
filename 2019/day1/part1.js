1e09a5856f43e993d33f938c72d85196913b8c4a
Cameron White <cameronwhite16@gmail.com>
2019-12-01 12:35:10 -0700

    dropping unused function

--- 2019/day1/part1.js
+++ 2019/day1/part1.js
@@ -4,13 +4,9 @@ function getFuelFromModuleMass(mass) {
     return Math.floor(mass / 3) - 2;
 }
 
-function computeTotalMassPart1(massList) {
-    return computeTotalMass(massList, getFuelFromModuleMass)
-}
-
 (function() {
     readInput("../input/day1/input")
-    .then(computeTotalMassPart1)
+    .then((massList) => computeTotalMass(massList, getFuelFromModuleMass))
     .then(logResult)
     .catch(logError)
 })();
--- 2019/day1/part2.js
+++ 2019/day1/part2.js
@@ -5,13 +5,9 @@ function getFuelFromMass(mass) {
     return fuel <= 0 ? 0 : (fuel + getFuelFromMass(fuel));
 }
 
-function computeTotalMassPart2(massList) {
-    return computeTotalMass(massList, getFuelFromMass)
-}
-
 (function() {
     readInput("../input/day1/input")
-    .then(computeTotalMassPart2)
+    .then((massList) => computeTotalMass(massList, getFuelFromMass))
     .then(logResult)
     .catch(logError)
 })()
