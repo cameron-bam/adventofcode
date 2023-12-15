05083ab8943274f3c4abfd7dca4cc74a65504309
Cameron White <cameronwhite16@gmail.com>
2023-12-15 00:00:42 -0700

    Day twelve part two

--- 2023/src/solutions/day_twelve.clj
+++ 2023/src/solutions/day_twelve.clj
@@ -1,20 +1,7 @@
 (ns day-twelve
   (:require [clojure.string :as str]
-            [lib.solution-registry :refer [def-solution]]))
-
-(def input "???.### 1,1,3
-.??..??...?##. 1,1,3
-?#?#?#?#?#?#?#? 1,3,1,6
-????.#...#... 4,1,1
-????.######..#####. 1,6,5
-?###???????? 3,2,1")
-
-(def solution "#.#.### 1,1,3
-.#...#....###. 1,1,3
-.#.###.#.###### 1,3,1,6
-####.#...#... 4,1,1
-#....######..#####. 1,6,5
-.###.##....# 3,2,1")
+            [lib.solution-registry :refer [def-solution]]
+            [clojure.math :refer [pow]]))
 
 (defn parse-groups [s]
   (->> (str/split s #",")
@@ -47,28 +34,40 @@
              (new-group-size spring group-size)
              (new-group groups spring group-size)))))
 
-(defn count-possible-groups
-  ([[springs groups]]
-   (count-possible-groups springs groups 0))
-  ([[spring & springs] groups group-size]
-   (if (nil? spring)
-    (do
-      #_(prn "reached end of row!" group-size groups)
-      (if (or (and (= group-size (first groups)) (= 1 (count groups)))
-              (and (= 0 group-size) (nil? (seq groups))))
-        1 0))
-    (if (= "?" spring)
-     (do 
-       #_(prn "Handling unknown value" spring springs groups group-size)
-       (+ (count-possible-groups (cons "." springs) groups group-size)
-        (count-possible-groups (cons "#" springs) groups group-size)))
-     (let [new-group-size (new-group-size spring group-size)
-           cur-group (or (first groups) 0)
-           exited-group? (and (= new-group-size 0) (not= 0 group-size))]
-       #_(prn "Checking if new-group-size is valid" new-group-size groups)
-       (if (or (> new-group-size cur-group)
-               (and exited-group? (not= cur-group group-size)))
-         0 (recur springs (if exited-group? (rest groups) groups) new-group-size)))))))
+(def count-possible-groups
+  (memoize
+   (fn ([[springs groups]] 
+        (count-possible-groups springs groups 0))
+     ([[spring & springs] groups group-size]
+      (if (nil? spring)
+        (if (or (and (= group-size (first groups)) (= 1 (count groups)))
+                (and (= 0 group-size) (nil? (seq groups))))
+          1 0)
+        (if (= "?" spring)
+          (+ (count-possible-groups (cons "." springs) groups group-size)
+             (count-possible-groups (cons "#" springs) groups group-size))
+          (let [new-group-size (new-group-size spring group-size)
+                cur-group (or (first groups) 0)
+                exited-group? (and (= new-group-size 0) (not= 0 group-size))] 
+            (if (or (> new-group-size cur-group)
+                    (and exited-group? (not= cur-group group-size)))
+              0 (recur springs (if exited-group? (rest groups) groups) new-group-size)))))))))
+
+
+(defn join-seqs [separator seqs]
+  (loop [[s & seqs] seqs
+         new-seq []]
+    (if (nil? seqs)
+      (concat new-seq s)
+      (recur seqs (concat new-seq (conj (apply vector s) separator))))))
+
+(defn unfold-line [unfold-count [springs groups]]
+  (let [copy-times (fn [coll]
+                     (->> (range 0 unfold-count)
+                          (map (fn [_] coll))))]
+    [(->> springs (copy-times) (join-seqs "?"))
+     (->> (copy-times groups)
+          (apply concat))]))
 
 (defn solve-part-one [s]
   (->> s
@@ -77,5 +76,14 @@
        (map count-possible-groups)
        (reduce + 0)))
 
+(defn solve-part-two [s]
+  (->> s
+       (str/split-lines)
+       (map parse-line)
+       (map (comp count-possible-groups (partial unfold-line 5)))
+       (reduce +' 0)
+       bigint))
+
 (def-solution
-  (solve-part-one (slurp "./input/day_twelve.txt")))
+  (solve-part-one (slurp "./input/day_twelve.txt"))
+  (solve-part-two (slurp "./input/day_twelve.txt")))
